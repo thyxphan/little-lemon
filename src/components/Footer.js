77e949215d375bd0d08747a845ba3d07a3262b62
Thy Phan <thyphan@Thy-X-Phan.local>
2025-12-12 21:28:02 -0600

    Add SEO, OGP, and semantic HTML

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/menu">Menu</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+
+      <p>&copy; 2025 Little Lemon. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <h2>Homepage</h2>
+      <p>Welcome to Little Lemon!</p>
+    </main>
+  );
+}
+
+export default Main;
